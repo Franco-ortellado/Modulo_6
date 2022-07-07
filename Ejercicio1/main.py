d88acf0e05bd7a29427772f741ea6071219da62d
Franco-ortellado <ortelladp.frank@gmail.com>
2022-07-07 17:48:23 -0300

    update

---
+++ Ejercicio1/main.py
@@ -0,0 +1,10 @@
+import Calculadora
+
+
+a = int(input("ingrese un numero entero:"))
+b = int(input("ingrese otro un numero entero:"))
+
+print("suma:",Calculadora.suma(a,b))
+print("resta:",Calculadora.resta(a,b))
+print("multiplicacion:",Calculadora.multi(a,b))
+print("division:",Calculadora.divide(a,b))
