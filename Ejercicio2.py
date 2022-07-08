b3b26701967884a69a70e521907d05ebe7d5b343
Franco-ortellado <ortelladp.frank@gmail.com>
2022-07-07 22:48:30 -0300

    updt

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -3,7 +3,7 @@ import time
 hora = time.strftime("%H")
 min = time.strftime("%M")
 
-if (int(hora) >= 23):
+if (int(hora) >= 19):
     print("ve a casa")
 else:
-    print("falta {}hs y {}min. para ir a casa".format(22-int(hora),59-int(min)))
+    print("falta {}hs y {}min. para ir a casa".format(18-int(hora),59-int(min)))
